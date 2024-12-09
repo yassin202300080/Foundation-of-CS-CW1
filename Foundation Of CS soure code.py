90f76b3984a76f4aed6e1ea8a4c30d2f557d0d78
yassin202300080 <yassin.elkholy@gmail.com>
2024-12-10 00:06:50 +0200

    Adding error handling: To aviod key Generator Selection creat an infifnte loop using try-except

--- Foundation Of CS soure code.py
+++ Foundation Of CS soure code.py
@@ -10,29 +10,35 @@ def is_prime_number(num):
     return True
 #creating keys: public and private using random prime numbers
 def generate_keys():
-    #Finding random prime number
-    while True:
-        prime = random.randint(10, 100)
-        if is_prime_number(prime):
-            break
+    try:
+        #Finding random prime number
+        while True:
+            prime = random.randint(10, 100)
+            if is_prime_number(prime):
+                break
 
-    #Finding suitable generator
-    generator  = None
-    for g in range(2 , prime):
-        is_generator = True
-        for q in range(2 , prime):
-            if is_prime_number(q):
-                if pow(g , (prime - 1) // q , prime) == 1:
-                    is_generator = False
-                    break
-        if is_generator :
-            generator = g
-            break
+        #Finding suitable generator
+        generator  = None
+        for g in range(2 , prime):
+            is_generator = True
+            for q in range(2 , prime):
+                if is_prime_number(q):
+                    if pow(g , (prime - 1) // q , prime) == 1:
+                        is_generator = False
+                        break
+            if is_generator :
+                generator = g
+                break
 
-    #Generating private key
-    private_key = random.randint(1 , prime - 2)
+        #Generating private key
+        private_key = random.randint(1 , prime - 2)
 
-    #The public key component calculation
-    public_key_component = pow(generator, private_key, prime)
+        #The public key component calculation
+        public_key_component = pow(generator, private_key, prime)
 
-    return (prime , generator , public_key_component) , private_key
+        return (prime, generator, public_key_component), private_key
+
+    except Exception as e:
+        print(f"No Suitable Generator Found: {e}")
+
+
